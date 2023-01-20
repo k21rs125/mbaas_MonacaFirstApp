f9110f5ed6adb179bdfb130073b76e10c4baade4
k21rs125 <k21rs125@st.kyusan-u.ac.jp>
2023-01-20 14:16:59 +0900

    的を回転させるアニメーション

--- www/js/tapGame.js
+++ www/js/tapGame.js
@@ -59,6 +59,10 @@ function saveScore(name, score) {
     // ********************************************************
 }
 
+function ani1() {
+    $('.anim-box').addClass('animated rotato'); //アニメーション用のクラスを追加
+}
+
 // タイマー
 function countTime(time) {
     if (time > 0) {
@@ -104,4 +108,22 @@ function tapCount() {
         this.counter += 1;
         $("#list-page strong").html(String(this.counter));
     }
+}
+
+function rankingTable() {
+    // 保存先クラスを作成
+    var highScore = ncmb.DataStore("GameScore");
+    // scoreの降順でデータ5件を取得するように設定する
+    highScore.order("score", true)
+        .limit(5).fetchAll()
+        .then(function (results) {
+            // 検索に成功した場合の処理
+            console.log("検索に成功しました。");
+            // テーブルにデータをセット
+            setData(results);
+        })
+        .catch(function (error) {
+            // 検索に失敗した場合の処理
+            console.log("検索に失敗しました。エラー:" + error);
+        });
 }
